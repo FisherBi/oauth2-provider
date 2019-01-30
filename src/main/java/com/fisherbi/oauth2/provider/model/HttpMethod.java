694a6a835e92f71692c902fabbfee3f2fe018daf
fisbii <fisher.bi@leandev.se>
2019-01-30 17:57:03 +0800

    Add Http method

--- src/main/java/com/fisherbi/oauth2/provider/model/HttpMethod.java
+++ src/main/java/com/fisherbi/oauth2/provider/model/HttpMethod.java
@@ -3,5 +3,30 @@ package com.fisherbi.oauth2.provider.model;
 /**
  * Created by FisherBi on 2019/1/28.
  */
-public class HttpMethod {
+public enum  HttpMethod {
+
+    GET(false), POST(true), PUT(true), DELETE(false, true), HEAD(false), OPTIONS(false), TRACE(false), PATCH(true);
+
+    private final boolean requiresBody;
+    private final boolean permitBody;
+
+    HttpMethod(boolean requiresBody) {
+        this(requiresBody, requiresBody);
+    }
+
+    HttpMethod(boolean requiresBody, boolean permitBody) {
+        if (requiresBody && !permitBody) {
+            throw new IllegalArgumentException();
+        }
+        this.requiresBody = requiresBody;
+        this.permitBody = permitBody;
+    }
+
+    public boolean isRequiresBody() {
+        return requiresBody;
+    }
+
+    public boolean isPermitBody() {
+        return permitBody;
+    }
 }
